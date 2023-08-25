f1233213486cdff8854038afa410061f112c53a0
Curtis Myzie <curtis.myzie@gmail.com>
2023-08-25 15:08:55 -0400

    Clean up deps

--- cmd/yabs/eval.go
+++ cmd/yabs/eval.go
@@ -20,17 +20,14 @@ import (
 	"github.com/risor-io/risor/parser"
 	"github.com/risor-io/risor/vm"
 
-	modAws "github.com/risor-io/risor/modules/aws"
 	modBase64 "github.com/risor-io/risor/modules/base64"
 	modBytes "github.com/risor-io/risor/modules/bytes"
 	modFetch "github.com/risor-io/risor/modules/fetch"
 	modFmt "github.com/risor-io/risor/modules/fmt"
 	modHash "github.com/risor-io/risor/modules/hash"
-	modImage "github.com/risor-io/risor/modules/image"
 	modJson "github.com/risor-io/risor/modules/json"
 	modMath "github.com/risor-io/risor/modules/math"
 	modOs "github.com/risor-io/risor/modules/os"
-	modPgx "github.com/risor-io/risor/modules/pgx"
 	modRand "github.com/risor-io/risor/modules/rand"
 	modStrconv "github.com/risor-io/risor/modules/strconv"
 	modStrings "github.com/risor-io/risor/modules/strings"
@@ -209,22 +206,17 @@ func getBuiltins(bs *yabs.Yabs) map[string]object.Object {
 		"time":    modTime.Module(),
 		"rand":    modRand.Module(),
 		"strconv": modStrconv.Module(),
-		"pgx":     modPgx.Module(),
 		"uuid":    modUuid.Module(),
 		"os":      modOs.Module(),
 		"bytes":   modBytes.Module(),
 		"base64":  modBase64.Module(),
 		"fmt":     modFmt.Module(),
-		"image":   modImage.Module(),
 		// custom builtins
 		"register": object.NewBuiltin("register", registerFunc(bs)),
 		"sh":       object.NewBuiltin("sh", sh),
 		"fs":       object.NewBuiltin("fs", fsFunc(bs)),
 		"go":       object.NewBuiltin("go", goTcFunc(bs)),
 		"node":     object.NewBuiltin("node", nodeTcFunc(bs)),
-	}
-	if awsMod := modAws.Module(); awsMod != nil {
-		allBuiltins["aws"] = awsMod
 	}
 
 	// default builtins
